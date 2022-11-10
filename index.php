3b2a8bbc49aa17b284054fbc954d12ec2a4fc421
bonapartenapo <bonapartenapoleon58@gmail.com>
2022-11-10 23:38:18 +0100

    initilisation et ajout des fichier

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>acceuil
+    </title>
+    <link rel="stylesheet" href="css/index.css">
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+
+</head>
+<body>
+    <!-- entête -->
+    <?php include('header.php') ?>
+    <!-- fin de l'entête -->
+<!-- debut partir body -->
+<div class="container">
+       <div class="part1">
+                
+                <h2 class="taile-titless police-titles"> Développez votre niveau <br class="title-2"> en programmation</h2>
+
+            <p class="taille-sous-titles police-sous-titles  depla">
+                Rejoignez la meilleure communauté de programmers  au <br>
+                monde,devenez épanoui,trouvez votre job, rencontrez <br> 
+                des personnes comme vous <br>
+                <button class="btn-secondary btn-text">Intégrer Maintenant</button>
+            </p>
+                <img class ="img-body" src="img/index/body/body.png">
+        </div>
+
+
+        <div class="mini-div">
+        <div class="mini-div2">
+                <img class="tof-jobs" src="img/index/body/jobs.png">
+                <a href="#" class="jobs1">Trouve ton jobs</a>
+                <a href="#" class="developeur">devolope competence</a>
+                <a href="#" class="jsobs2">Trouve ton jobs</a>
+                <a href="#" class="jsobs3">Trouve ton jobs</a>
+                <a href="#" class="jsobs4">Trouve ton jobs2</a>
+              
+        </div>
+<div class="part1">
+    <img class ="boy" src="img/index/body/boy.png" >
+    <p class="new-techno">
+        <h1 class="technologie"> Envie d’appndre une nouvelle <br> technologie  ?</h1>
+        <div class="sous-texte">
+             La communauté TPH  a mit  en place un
+             système de recommandation de tutoriel 
+             recommandés, concernant tous les langages
+             de ton choix
+        </div>
+        <button class="btn-secondary btn-text btn-tuto">Visiter les tutoriels</button>
+    </p>
+</div>
+
+<div class="part2">
+    <div class="mini-part1">
+  <main class="main2">
+      <h1 class="trouver-job"> Le <b style =" color: #357DED;"> meilleur</b> moyen de trouvez un <br>Job  pour vous</h1>
+      <p class ="texte1">
+      Etes vous compétent ? La communauté TPH en collaboration
+      avec plusieurs entreprise  , vous propose des jobs , de 
+      nombreux jobs, débrouillez vous à postuler 😚      
+      </p>
+
+      <button class="btn-secondary btn-text">Voir les opportunités</button>
+      </main>
+      <img class ="tof-entreprise" src="img/index/body/entreprise.jpg" alt="">
+      </div>
+
+
+<center>
+<div class="communaute">
+    <h1 class="intergrer-cmunaute">Envie d’intégrer la communauté  ?</h1>
+    <p class="texte">
+       La communauté TPH S’étend sur plusieurs plateformes , à votre choix, et votre gout,<br>
+       rencontrez des personnes , de pays, continent différents 
+
+    </p>
+    <button class="btn-secondary btn-text btn-whatsapp">intergrer via Whatsapp</button>
+       <button class="btn-primary btn-text btn-discord">integrer via Discord</button>
+</div>
+</center>
+
+<h1 class="neuf">Quoi de <b class = "color-neuf">neuf</b> à TPH  ?</h1>
+
+</div>
+</div>
+</div>
+<footer class="footer">
+    <div class="div1">
+        <div class="tph1">
+            <h2 class="web">TheProgrammer , web <br> ux designer</h2>
+        <p class ="lorem-footer">
+            Lorem Iping industry. Lorem Ipsum has
+            been the industry's standard dummy text
+            ever since the 1500s, when a. ..
+
+            <img class="livre" src="icone-logo/body/vector.png" >
+            <a href="#" class ="lecture-article">Lire cet  article</a>
+        </p>
+        </div>
+        <img class ="img-programmer"src="img/index/body/programmer.png" alt="meliodas">
+    </div>
+
+
+    <!--  -->
+    <div class="div2">
+        <div class="tph1">
+            <h2 class="web">TheProgrammer , web <br> ux designer</h2>
+            <p class ="lorem-footer">
+            Lorem Iping industry. Lorem Ipsum has
+            been the industry's standard dummy text
+            ever since the 1500s, when a. ..
+
+            <img class="livre" src="icone-logo/body/vector.png" >
+            <a href="#" class ="lecture-article">Lire cet  article</a>
+        </p>
+        </div>
+        <img class ="img-programmer"src="img/index/body/programmer.png" alt="meliodas">
+    </div>
+
+    <div class="div3">
+        <div class="tph1">
+            <h2 class="web">TheProgrammer , web <br> ux designer</h2>
+            <p class ="lorem-footer">
+            Lorem Iping industry. Lorem Ipsum has
+            been the industry's standard dummy text
+            ever since the 1500s, when a. ..
+
+            <img class="livre" src="icone-logo/body/vector.png" >
+            <a href="#" class ="lecture-article">Lire cet  article</a>
+        </p>
+        </div>
+        <img class ="img-programmer"src="img/index/body/programmer.png" alt="meliodas">
+    </div>
+</footer>
+
+</body>
+</html>
